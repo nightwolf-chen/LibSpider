811c2342924058ccf40e4468619cff4c76d3062c
Bruce Chen <466202783@qq.com>
2014-03-11 20:14:08 +0800

    实现借阅列表页面的解析

--- src/libspider/LibSpider.java
+++ src/libspider/LibSpider.java
@@ -7,10 +7,13 @@ package libspider;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.Map;
 import network.HttpClientAdaptor;
+import object.BookBorrowHistory;
+import object.User;
+import util.PageParserBorrowList;
 import util.PageParserUserInfo;
-
 
 /**
  *
@@ -18,17 +21,29 @@ import util.PageParserUserInfo;
  */
 public class LibSpider {
 
-   
     public static void main(String[] args) throws MalformedURLException, IOException {
         // TODO code application logic here
-       String userid = "20101003714";
+        String userid = "20101003714";
+
+        HttpClientAdaptor httpClient = new HttpClientAdaptor();
+        PageParserUserInfo userInfoPaser = new PageParserUserInfo(userid, httpClient);
+
+        Map<String, String> userInfo = userInfoPaser.parserPageForData();
+
+        User user = new User(userid,
+                userInfo.get(PageParserUserInfo.kName),
+                userInfo.get(PageParserUserInfo.kCollege),
+                userInfo.get(PageParserUserInfo.kMajor));
        
-       HttpClientAdaptor httpClient = new HttpClientAdaptor();
-       PageParserUserInfo userInfoPaser = new PageParserUserInfo(userid,httpClient);
+        String userBorrowListUrl = userInfo.get(PageParserUserInfo.kBorrowHistoryUrl);
         
-       Map<String,String> userInfo = userInfoPaser.parserPageForData();
+        PageParserBorrowList borrowListPaser = new PageParserBorrowList(userBorrowListUrl,userid, httpClient);
+        ArrayList<Object> list =  borrowListPaser.parserPageForRepeatedData();
         
-       System.out.print(userInfo);
+        BookBorrowHistory bookBorrowHistory = (BookBorrowHistory)list.get(0);
+        System.out.println(userInfo);
+        System.out.println(list.size());
+        System.out.println(bookBorrowHistory.getaBook().getBookName());
     }
 
 }
--- src/util/PageParserBorrowList.java
+++ src/util/PageParserBorrowList.java
@@ -18,16 +18,25 @@ package util;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import network.HttpClientAdaptor;
+import object.Book;
+import object.BookBorrowHistory;
 
 /**
  *
  * @author bruce
  */
 public class PageParserBorrowList extends PageParser{
-
-    public PageParserBorrowList(HttpClientAdaptor httpClient) {
+    
+    private final String url;
+    private final String userid;
+    
+    public PageParserBorrowList(String url,String userid,HttpClientAdaptor httpClient) {
         super(httpClient);
+        this.url = url;
+        this.userid = userid;
     }
 
     @Override
@@ -37,7 +46,43 @@ public class PageParserBorrowList extends PageParser{
 
     @Override
     public ArrayList<Object> parserPageForRepeatedData() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Object> list = new ArrayList<>();
+        
+        String pageContent = this.httpClient.doGet(this.url);
+        
+        String regex = " <tr> \n" +
+        "  <td class=td1 id=centered valign=top><A HREF=(.*?)>(.*?)</A></td> \n" +
+        "  <td class=td1 valign=top><a href=\"(.*?)\" target=_blank>(.*?)</a></td> \n" +
+        "  <td class=td1 valign=top><a href=\"(.*?)\" target=_blank>(.*?)</a></td> \n" +
+        "  <td class=td1 valign=top>(.*?)</td> \n" + //7
+        "  <td class=td1 valign=top>(.*?)</td> \n" +
+        "  <td class=td1 valign=top>(.*?)</td> \n" +
+        "  <td class=td1 valign=top>(.*?)</td> \n" +
+        "  <td class=td1 valign=top>(.*?)</td> \n" +
+        "  <td class=td1 valign=top>(.*?)</td> \n" +
+        "  <td class=td1 valign=top>(.*?)</td> \n" +
+        " </tr> ";
+        
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pageContent);
+        
+        while(matcher.find()){
+            String borrowInfoUrl = matcher.group(1);
+            String bookDetailUrl = matcher.group(3);
+            String author = matcher.group(4);
+            String bookName = matcher.group(6);
+            String publishTime = matcher.group(7);
+            String shouldReturnTime = matcher.group(8) + matcher.group(9);
+            String actuelReturnTime = matcher.group(10) + matcher.group(11);
+            String location = matcher.group(13);
+            
+            Book aBook = new Book(bookName, author, publishTime, location, bookDetailUrl);
+            BookBorrowHistory borrowHistory = new BookBorrowHistory(userid, aBook, shouldReturnTime, actuelReturnTime);
+            list.add(borrowHistory);
+        }
+        
+        
+        return list;
     }
     
 }
